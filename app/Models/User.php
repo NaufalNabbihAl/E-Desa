dd6d087a87dd9e80685e09d67c5f10526b308ac2
Rifqy Attaufi <rifqy1406@gmail.com>
2023-09-22 00:59:26 +0700

    change user pk to id

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,8 +12,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $primaryKey = "NIK";
-
     /**
      * The attributes that are mass assignable.
      *
--- database/migrations/2023_07_16_040558_create_users_table.php
+++ database/migrations/2023_07_16_040558_create_users_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('NIK')->primary();
+            $table->id();
+            $table->string('NIK')->unique();
             $table->enum('role', ['warga', 'admin', 'rw', 'rt']);
             $table->string('password');
             $table->rememberToken();
